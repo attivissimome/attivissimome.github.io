9aa2594212e250c5514c9ef2fb98e3b69178e312
Filippo Vitale <fvitale@gmail.com>
2014-06-28 19:32:25 +1000

    release/publish script

---
+++ update_ghpages.sh
@@ -0,0 +1,28 @@
+#!bin/bash/ -e
+
+SOURCE_BRANCH=develop
+PUBLISH_BRANCH=master
+ 
+if git status --porcelain 2>/dev/null | grep -q .; then
+  echo "Working copy is dirty" >&2
+  exit 1
+fi
+ 
+# task for release
+grunt release
+ 
+git checkout $PUBLISH_BRANCH
+ 
+# remove all folders expcept dist (and all dotfiles)
+ls -1 | grep -v -E 'dist' | xargs rm -R
+
+# copy all files from dist to root
+cd dist && cp -rf * ../
+cd ../ && rm -rf dist
+ 
+# add all needed files and push it into origin/gh-pages
+git add -A
+git commit -m "Publish commit"
+git push origin $PUBLISH_BRANCH
+ 
+git checkout $SOURCE_BRANCH
